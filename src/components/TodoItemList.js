bcc6ac2c972ac77df598ee28067e24be4e5b9f32
pebblepark <subling02@gmail.com>
2021-01-22 13:53:27 +0900

    TodoItemList 컴포넌트 생성

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+// 3 props
+// 1. todos : An array of objects.
+// 2. onToggle : Functions to turn the checkbox on and off.
+// 3. onRemove : Functions to delete an item.
+
+// Rendering multiple TodoItem components
+// When rendering a 'List', use it as a class type component rather than a functional type for component optimization
+
+class TodoItemList extends Component {
+    render() {
+        const {todos, onToggle, onRemove } = this.props;
+        return (
+            <div>
+
+            </div>
+        );
+    };
+}
+
+export default TodoItemList;

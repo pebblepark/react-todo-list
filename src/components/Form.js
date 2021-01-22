315e5a71f8268c2a5aa12cd21dd536e0ed89516a
pebblepark <subling02@gmail.com>
2021-01-22 13:40:08 +0900

    Form 생성

---
+++ src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Form.css';
+
+// 4 props
+// 1. value : input value
+// 2. onCreate : button click event
+// 3. onChange : value change event
+// 4. onKeyPress : key press event from input
+//                *For this event, when the Enter key is pressed, it executes the same action as onCreate.*
+
+const Form = ({value, onChange, onCreate, onKeyPress}) => {
+    return (
+        <div className="form">
+            <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
+            <div class="create-button" onClick={onCreate}>
+                Add
+            </div>
+        </div>
+    );
+};
+
+export default Form;
